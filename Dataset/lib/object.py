3ec76a0623b10e02832977ff87a2b6106c368d79
robtu328 <robtu328@yahoo.com.tw>
2020-11-29 23:59:02 +0800

    add Image file

--- Dataset/lib/object.py
+++ Dataset/lib/object.py
@@ -1,17 +1,21 @@
 import xml.etree.ElementTree as ET
-
+from PIL import Image
+import os, sys
+import imghdr
+import argparse
 
 class XMLObject:
   def __init__ (self, Ent, img):
     bndbox=Ent.find('bndbox')
     self.startx=int(bndbox.find('xmin').text)
     self.starty=int(bndbox.find('ymin').text)
-    self.width=int(bndbox.find('xmax').text) - self.startx
-    self.height=int(bndbox.find('ymax').text) - self.starty
+    self.endx=int(bndbox.find('xmax').text)
+    self.endy=int(bndbox.find('ymax').text) 
     self.name = Ent.find('name').text
     self.pose = Ent.find('pose').text
     self.img = img
-
+  def crop():
+    return self.img.crop()
 
 class XMLImage:
   def __init__ (self, imgEnt, img, path=""):
@@ -35,16 +39,11 @@ class XMLImage:
        self.objList.append(XMLObject(obj, img))
     
     for obj in self.objList:
-      print(obj.startx, obj.starty, obj.width, obj.height, obj.name)
+      print(obj.startx, obj.starty, obj.endx, obj.endy, obj.name)
 
 
 
 img=0
 xmlfile=ET.parse("../2v2Label/IMG_6098 003.xml")
+img = Image.open("../2v2Image/IMG_6098 003.jpg")
 xmlImage=XMLImage(xmlfile, img, "./")
-
-
- 
-
-
-

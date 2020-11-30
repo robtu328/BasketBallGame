17f4c8a82e7947dd8be971fb6793805f98c6a23a
robtu328 <robtu328@yahoo.com.tw>
2020-11-30 11:51:06 +0800

    Update object.py

--- Dataset/lib/object.py
+++ Dataset/lib/object.py
@@ -14,17 +14,20 @@ class XMLObject:
     self.name = Ent.find('name').text
     self.pose = Ent.find('pose').text
     self.img = img
-  def crop():
-    return self.img.crop()
+  def crop(self):
+    return self.img.crop((self.startx, self.starty, self.endx, self.endy))
 
 class XMLImage:
-  def __init__ (self, imgEnt, img, path=""):
+  def __init__ (self, imgEnt, img, xmlPath="", imgPath="", savePath=""):
     self.imgName=imgEnt.find('filename').text
-    if (path == ""):
-      self.path=imgEnt.find('path').text
+    if (xmlPath == ""):
+      self.imgNamemgPath=imgEnt.find('path').text
     else:
-      self.path=path
+      self.imgPath=imgPath
       
+    self.xmlPath=xmlPath
+    self.savePath=savePath
+
     imgSource=imgEnt.find('source')
     self.dBaseName=imgSource.find('database').text
     
@@ -42,8 +45,19 @@ class XMLImage:
       print(obj.startx, obj.starty, obj.endx, obj.endy, obj.name)
 
 
+  def saveObjectImage (self, savelist=[]):
+     f, e = os.path.splitext(self.savePath+self.imgName)
+     print(f, e)
+     for obj in self.objList:
+      print(f+'_'+obj.name+e)
+      cropImg=obj.crop()
+      cropImg.save(f+'_'+obj.name+e)
+      cropImg.show()
+
+    
 
 img=0
 xmlfile=ET.parse("../2v2Label/IMG_6098 003.xml")
 img = Image.open("../2v2Image/IMG_6098 003.jpg")
-xmlImage=XMLImage(xmlfile, img, "./")
+xmlImage=XMLImage(xmlfile, img, "./", "../2v2Image/", "../2v2Image/")
+xmlImage.saveObjectImage()
